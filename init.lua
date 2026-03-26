e391bc7ce2102523343486108e18e26c328a78fd
LPeter1997 <lenkefi.peti@gmail.com>
2026-03-26 22:08:56 +0100

    Remove copilot chat and separate roslyn integration

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ vim.opt.fillchars = {
 -- Allow for per-project configuration
 vim.o.exrc = true
 vim.o.secure = true
+
+-- Use <Esc> to exit terminal mode
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 --------------------------------------------------------------------------------
 
 -- Dotnet-specific build function
@@ -193,23 +196,6 @@ require('lazy').setup({
     {
       'github/copilot.vim',
       lazy = false,
-    },
-    -- Copilot chat
-    {
-      'CopilotC-Nvim/CopilotChat.nvim',
-      dependencies = {
-        { 'nvim-lua/plenary.nvim', branch = 'master' },
-      },
-      build = 'make tiktoken',
-      opts = {
-        model = 'gpt-4.1', -- AI model to use
-        temperature = 0.1, -- Lower = focused, higher = creative
-        window = {
-          layout = 'vertical', -- 'vertical', 'horizontal', 'float'
-          width = 0.3, -- 30% of screen width
-        },
-        auto_insert_mode = true, -- Enter insert mode when opening
-      },
     },
     -- Telescope for fuzzy finding basically everything
     {
@@ -462,13 +448,6 @@ require('lazy').setup({
           'github:Crashdummyy/mason-registry',
         },
       },
-    },
-    -- C# language support
-    {
-      'seblyng/roslyn.nvim',
-      --- @module 'roslyn.config'
-      --- @type RoslynNvimConfig
-      opts = {},
     },
     -- Generic .NET tool integration
     {
